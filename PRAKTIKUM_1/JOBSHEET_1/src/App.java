23bd736ea51ac1d5c3a3e2dd16c93e6110c28bf4
EiyuEfendi <eiyuefendi12@gmail.com>
2025-02-16 18:52:07 +0700

    first commit

---
+++ PRAKTIKUM_1/JOBSHEET_1/src/App.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Program Menghitung Nilai Akhir");
+        System.out.println("======================");
+        System.out.println("Masukan Nilai Tugas: ");
+        System.out.println("Masukan Nilai Kuis: ");
+        System.out.println("Masukan Nilai UTS: ");
+        System.out.println("Masukan Nilai UAS: ");
+        System.out.println("======================");
+        System.out.println("======================");
+
+       
+    }
+}
