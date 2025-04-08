3818a1dcec46e6923d14942cc51c544304b0780e
eiyu18 <eiyuefendi12@gmail.com>
2025-04-08 22:42:45 +0700

    Add files via upload

---
+++ JOBSHEET_7/src/LecturerData.java
@@ -0,0 +1,84 @@
+public class LecturerData {
+    Lecturer[] lecturerData;
+    int idx;
+
+    public LecturerData() {
+        lecturerData = new Lecturer[10];
+        idx = 0;
+    }
+
+    public void add(Lecturer dsn) {
+        if (idx < lecturerData.length) {
+            lecturerData[idx] = dsn;
+            idx++;
+        } else {
+            System.out.println("Data is full!");
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < idx; i++) {
+            lecturerData[i].print();
+        }
+    }
+
+    public void sortingASC() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 0; j < idx - i - 1; j++) {
+                if (lecturerData[j].age > lecturerData[j + 1].age) {
+                    Lecturer temp = lecturerData[j];
+                    lecturerData[j] = lecturerData[j + 1];
+                    lecturerData[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void sortingDSC() {
+        for (int i = 0; i < idx - 1; i++) {
+            int maxIdx = i;
+            for (int j = i + 1; j < idx; j++) {
+                if (lecturerData[j].age > lecturerData[maxIdx].age) {
+                    maxIdx = j;
+                }
+            }
+            Lecturer temp = lecturerData[i];
+            lecturerData[i] = lecturerData[maxIdx];
+            lecturerData[maxIdx] = temp;
+        }
+    }
+
+    public Lecturer sequentialSearch(String keyword) {
+        if (idx == 0) {
+            System.out.println("No data available.");
+            return null;
+        }
+        for (int i = 0; i < idx; i++) {
+            if (lecturerData[i].id.equalsIgnoreCase(keyword) || lecturerData[i].name.equalsIgnoreCase(keyword)) {
+                return lecturerData[i];
+            }
+        }
+        return null; 
+    }
+
+    public Lecturer binarySearch(int targetAge) {
+        if (idx == 0) {
+            System.out.println("No data available.");
+            return null;
+        }
+        sortingASC(); 
+        int low = 0, high = idx - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (lecturerData[mid].age == targetAge) {
+                return lecturerData[mid];
+            } else if (lecturerData[mid].age < targetAge) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return null; 
+    }
+}
