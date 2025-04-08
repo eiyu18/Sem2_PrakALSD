be3187378b5ff47a6db81cf61b4ee79c15bd582c
eiyu18 <eiyuefendi12@gmail.com>
2025-04-08 16:25:31 +0700

    Update StudentDemo10.java

--- JOBSHEET_6/src/StudentDemo10.java
+++ JOBSHEET_6/src/StudentDemo10.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
+
 public class StudentDemo10 {
     public static void main(String[] args) {
-        TopStudent10 topStudent = new TopStudent10();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input the number of students to input: ");
+        int n = sc.nextInt();
+        TopStudent10 topStudents = new TopStudent10();
 
-        topStudent.add(new Student10("2201", "Alice", "A", 3.9));
-        topStudent.add(new Student10("2202", "Bob", "B", 3.7));
-        topStudent.add(new Student10("2203", "Charlie", "C", 3.8));
-        topStudent.add(new Student10("2204", "David", "D", 3.6));
-        topStudent.add(new Student10("2205", "Eve", "E", 4.0));
+        for (int i = 0; i < n; i++) {
+            System.out.println("Input student data " + (i + 1));
+            System.out.print("NIM: ");
+            String nim = sc.next();
+            System.out.print("Name: ");
+            String name = sc.next();
+            System.out.print("Class: ");
+            String studentClass = sc.next();
+            System.out.print("GPA: ");
+            double gpa = sc.nextDouble();
+            topStudents.add(new Student10(nim, name, studentClass, gpa));
+        }
 
-        System.out.println("Original student list:");
-        topStudent.print();
+        System.out.println("Original student List:");
+        topStudents.print();
 
-        topStudent.bubbleSort();
-        System.out.println("Sorted student list (by GPA, descending):");
-        topStudent.print();
+        topStudents.bubbleSort();
+        System.out.println("Sorted student List (by GPA, descending):");
+        topStudents.print();
+
     }
+
 }
